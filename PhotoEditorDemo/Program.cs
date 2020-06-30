bb5f658f815e8ade800dee43254b6c78a6a98631
aleksadjdj <aleksa.djdj@gmail.com>
2020-06-30 12:07:09 +0200

    Update Program.cs

--- PhotoEditorDemo/Program.cs
+++ PhotoEditorDemo/Program.cs
@@ -1,21 +1,39 @@
-﻿using System;
-using System.Diagnostics;
+using System;
+using System.IO;
 
 namespace PhotoEditorDemo
 {
+    /// <summary>
+    /// Wordpress preview and thumbnail images
+    /// Problem: unwanted gray pixels on white background on an image caused by compression 
+    /// this program helped me to fix ~12k images on wordpress site
+    /// by removing gray pixels on white background
+    /// </summary>
+    /// 
     public class Program
     {
         static void Main()
         {
-
-            string inputFile = @"D:\asd.jpg";
-            string outputFile = @"D:\demo2.png";
-
+            var currentDirectory = new DirectoryInfo(@"C:\NewFolder\uploads\2020\03");
+            FileInfo[] files = currentDirectory.GetFiles("*.jpg"); 
+            
+            int processedImages = 0;
             Console.WriteLine("Working...");
 
-            var stopwatch = Stopwatch.StartNew();
-            new ImageEditor(inputFile, outputFile);
-            Console.WriteLine($"{stopwatch.Elapsed}");
+            foreach (FileInfo file in files)
+            {
+                string inputFile = file.FullName;
+                string outputFile = file.FullName;
+                try
+                {
+                    new ImageEditor(inputFile, outputFile);
+                    Console.WriteLine($"{++processedImages}. {inputFile}");
+                }
+                catch (Exception e) 
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
         }
     }
 }
